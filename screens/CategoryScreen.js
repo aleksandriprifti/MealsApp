0babda0639941c3f1c5c0db7653c24a2757fbc31
aleksandriprifti <prifti.aleksandri@gmail.com>
2023-11-06 23:50:36 +0100

    First Commit

---
+++ screens/CategoryScreen.js
@@ -0,0 +1,34 @@
+import { FlatList, StyleSheet, SafeAreaView } from "react-native";
+import React from "react";
+import { CATEGORIES } from "../data/dummy-data";
+import CategoryGridTile from "../components/CategoryGridTile";
+
+const CategoryScreen = ({ navigation }) => {
+  const renderCategoryItem = (itemData) => {
+    const pressHandler = () => {
+      navigation.navigate("MealsOverview");
+    };
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+      />
+    );
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item.id}
+        renderItem={renderCategoryItem}
+        numColumns={2}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default CategoryScreen;
+
+const styles = StyleSheet.create({});
